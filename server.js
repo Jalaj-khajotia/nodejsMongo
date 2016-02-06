cb991581e242ccc4d43e6b124daee28adf11e6cf
jalaj <jalajiitr@gmail.com>
2016-02-06 11:42:14 +0530

    path package removed.

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
 var app = express();
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
